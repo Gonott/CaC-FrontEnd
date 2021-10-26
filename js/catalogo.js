af4704d8dc0e3a8ec070559a321e9902e84a16dc
Otto <gonott@gmail.com>
2021-10-26 19:58:16 -0300

    problema con utf 8

--- js/catalogo.js
+++ js/catalogo.js
@@ -13,11 +13,11 @@
             break;
         case 2:
             nombre = "Torta Deadpool";
-            desc = "Torta personalizada de Deadpool, la espada es de turr�n con base de chocolate y decorada con gomitas, kitkat y mini alfajores";
+            desc = "Torta personalizada de Deadpool, la espada es de turron con base de chocolate y decorada con gomitas, kitkat y mini alfajores";
             break;
         case 3:
             nombre = "Torta Red Velvet";
-            desc = "Es una deliciosa torta recubierta con queso crema dulce. Su nombre se traduce a �Terciopelo Rojo�, debido a su caracter�stico color rojo profundo o vinotinto y su suave textura.";
+            desc = "Es una deliciosa torta recubierta con queso crema dulce. Su nombre se traduce a Terciopelo Rojo, debido a su caracteristico color rojo profundo o vinotinto y su suave textura.";
             break;
         case 4:
             nombre = "Torta de Banana y Dulce de Leche";
@@ -25,11 +25,11 @@
             break;
         case 5:
             nombre = "Lemon Pie";
-            desc = "Deliciosa torta de base s�lda, rellena de una secreta y riquisima crema con limon y decorada con una cobertura de merengue y limon rallado.";
+            desc = "Deliciosa torta de base solda, rellena de una secreta y riquisima crema con limon y decorada con una cobertura de merengue y limon rallado.";
             break;
         case 6:
             nombre = "Triple CheeseCake, un postre cremoso e irresistible";
-            desc = "Torta con relleno de quesEl cheesecake, conocido tambi�n como pastel de queso, es un postre cl�sico de la cocina americana. Popular en todo el mundo, su principal ingrediente es el queso crema. Su fondo es crujiente y se obtiene al moler o triturar galletas y mezclarlas con mantequilla y az�car.";
+            desc = "Torta con relleno de quesEl cheesecake, conocido tambi�n como pastel de queso, es un postre cl�sico de la cocina americana. Popular en todo el mundo, su principal ingrediente es el queso crema. Su fondo es crujiente y se obtiene al moler o triturar galletas y mezclarlas con mantequilla y azucar.";
             break;
         case 7:
             nombre = "Torta Rogel";
@@ -37,11 +37,11 @@
             break;
         case 8:
             nombre = "Cremoso, suave e intenso. Tiramis�";
-            desc = "El tiramis� es un postre italiano muy popular a nivel internacional hecho a partir de un ingrediente s�lido y seco humedecido en caf�. Sin embargo, su nacimiento est� plagado de historias, leyendas y disputas que llegan hasta nuestros d�as, sin duda te va a encantar.";
+            desc = "El tiramisu es un postre italiano muy popular a nivel internacional hecho a partir de un ingrediente solido y seco humedecido en cafe. Sin embargo, su nacimiento esta plagado de historias, leyendas y disputas que llegan hasta nuestros d�as, sin duda te va a encantar.";
             break;
         case 9:
             nombre = "Torta de Frutillas y Crema";
-            desc = "Clasica e irresistible. Con su bizcocho de chocolate o vainilla y rellena de frutillas con crema es un cl�sico de los postres. Est� recubierta con crema batida y frutillas.";
+            desc = "Clasica e irresistible. Con su bizcocho de chocolate o vainilla y rellena de frutillas con crema es un clasico de los postres. Esta recubierta con crema batida y frutillas.";
             break;
         default:
     }
